ec22824ffbe08c60cc36dade662a8883b9b79534
emo-nikoloff <emobg321@gmail.com>
2026-01-31 02:05:12 +0200

    Update Matching Brackets.cs

--- C#/3. Programming Advanced/Advanced/1.1 Stacks and Queues - Lab/04. Matching Brackets/Matching Brackets.cs
+++ C#/3. Programming Advanced/Advanced/1.1 Stacks and Queues - Lab/04. Matching Brackets/Matching Brackets.cs
@@ -5,6 +5,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string input = Console.ReadLine();
+        Stack<int> stack = new();
+        for (int i = 0; i < input.Length; i++)
+        {
+            char character = input[i];
+            if (character == '(')
+            {
+                stack.Push(i);
+            }
+            else if (character == ')')
+            {
+                int startIndex = stack.Pop();
+                string contents = input.Substring(startIndex, i - startIndex + 1);
+                Console.WriteLine(contents);
+            }
+        }
     }
 }
