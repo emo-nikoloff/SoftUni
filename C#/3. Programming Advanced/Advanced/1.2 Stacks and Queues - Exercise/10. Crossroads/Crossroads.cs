549604598059a4c2e3762db1d0d39effc2a68fd9
emo-nikoloff <emobg321@gmail.com>
2026-02-05 19:51:02 +0200

    Update Crossroads.cs

--- C#/3. Programming Advanced/Advanced/1.2 Stacks and Queues - Exercise/10. Crossroads/Crossroads.cs
+++ C#/3. Programming Advanced/Advanced/1.2 Stacks and Queues - Exercise/10. Crossroads/Crossroads.cs
@@ -11,17 +11,70 @@ class Program
     static void Main(string[] args)
     {
         int greenLightDuration = int.Parse(Console.ReadLine());
-        int freeWindow = int.Parse(Console.ReadLine());
+        int freeWindowDuration = int.Parse(Console.ReadLine());
 
         Queue<string> cars = new();
 
         string input = string.Empty;
+
+        int passedCars = 0;
         while ((input = Console.ReadLine()) != "END")
         {
             if (input != "green")
             {
                 cars.Enqueue(input);
             }
+            else
+            {
+                bool isGreen = true;
+                int time = 0;
+                while (cars.Count > 0)
+                {
+                    if (isGreen)
+                    {
+                        string car = cars.Dequeue();
+                        Queue<char> carSymbols = new(car);
+
+                        while (carSymbols.Count > 0 && greenLightDuration - time > 0)
+                        {
+                            carSymbols.Dequeue();
+                            time++;
+                        }
+
+                        if (carSymbols.Count == 0)
+                        {
+                            passedCars++;
+                        }
+                        else
+                        {
+                            for (int i = freeWindowDuration - 1; i >= 0; i--)
+                            {
+                                carSymbols.Dequeue();
+                                if (carSymbols.Count == 0)
+                                {
+                                    passedCars++;
+                                    break;
+                                }
+                            }
+
+                            isGreen = false;
+
+                            if (carSymbols.Count != 0)
+                            {
+                                Console.WriteLine("A crash happened!");
+                                Console.WriteLine($"{car} was hit at {carSymbols.Peek()}.");
+                                return;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
         }
+        Console.WriteLine("Everyone is safe.");
+        Console.WriteLine($"{passedCars} total cars passed the crossroads.");
     }
 }
