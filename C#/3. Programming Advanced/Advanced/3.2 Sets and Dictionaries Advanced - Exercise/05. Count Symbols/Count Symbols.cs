f5ef7c9dc08d306beabd301696dd9a0c0de655aa
emo-nikoloff <emobg321@gmail.com>
2026-02-10 00:52:50 +0200

    New tasks

---
+++ C#/3. Programming Advanced/Advanced/3.2 Sets and Dictionaries Advanced - Exercise/05. Count Symbols/Count Symbols.cs
@@ -0,0 +1,25 @@
+﻿namespace _05._Count_Symbols;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        Dictionary<char, int> occurences = new();
+        for (int i = 0; i < input.Length; i++)
+        {
+            char character = input[i];
+            if (!occurences.ContainsKey(character))
+            {
+                occurences.Add(character, 1);
+                continue;
+            }
+            occurences[character]++;
+        }
+
+        foreach ((char character, int count) in occurences.OrderBy(c => c.Key))
+        {
+            Console.WriteLine($"{character}: {count} time/s");
+        }
+    }
+}
---
+++ C#/3. Programming Advanced/Advanced/3.2 Sets and Dictionaries Advanced - Exercise/06. Wardrobe/Wardrobe.cs
@@ -0,0 +1,48 @@
+﻿namespace _06._Wardrobe;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int colours = int.Parse(Console.ReadLine());
+
+        Dictionary<string, Dictionary<string, int>> clothes = new();
+        for (int i = 1; i <= colours; i++)
+        {
+            string[] input = Console.ReadLine().Split(new string[] { ",", " -> " }, StringSplitOptions.RemoveEmptyEntries);
+            string colour = input[0];
+
+            if (!clothes.ContainsKey(colour))
+            {
+                clothes.Add(colour, new());
+            }
+            foreach (string wear in input.Skip(1))
+            {
+                if (!clothes[colour].ContainsKey(wear))
+                {
+                    clothes[colour].Add(wear, 1);
+                    continue;
+                }
+                clothes[colour][wear]++;
+            }
+        }
+
+        string[] searchWear = Console.ReadLine().Split();
+        string wearColour = searchWear[0];
+        string wearType = searchWear[1];
+
+        foreach ((string colour, Dictionary<string, int> clothing) in clothes)
+        {
+            Console.WriteLine($"{colour} clothes:");
+            foreach ((string wear, int count) in clothing)
+            {
+                string suffix = string.Empty;
+                if (colour == wearColour && wear == wearType)
+                {
+                    suffix = " (found!)";
+                }
+                Console.WriteLine($"* {wear} - {count}{suffix}");
+            }
+        }
+    }
+}
