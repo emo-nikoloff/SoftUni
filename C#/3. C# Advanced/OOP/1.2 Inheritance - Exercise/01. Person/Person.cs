b6d02bebbf5fe8d20f4bc3a2c9fc95b0284d63cf
emo-nikoloff <emobg321@gmail.com>
2026-02-26 22:05:05 +0200

    Update Person.cs

--- C#/3. C# Advanced/OOP/1.2 Inheritance - Exercise/01. Person/Person.cs
+++ C#/3. C# Advanced/OOP/1.2 Inheritance - Exercise/01. Person/Person.cs
@@ -11,28 +11,8 @@ public class Person
         Age = age;
     }
 
-    public string Name
-    {
-        get
-        {
-            return name;
-        }
-        set
-        {
-            name = value;
-        }
-    }
-    public int Age
-    {
-        get
-        {
-            return age;
-        }
-        set
-        {
-            age = value;
-        }
-    }
+    public string Name { get => name; set => name = value; }
+    public int Age { get => age; set => age = value; }
 
     public override string ToString()
     {
