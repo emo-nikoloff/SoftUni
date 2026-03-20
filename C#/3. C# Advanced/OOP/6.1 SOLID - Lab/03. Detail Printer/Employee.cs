6f1f52be6396d6d4d7df32302554676970ba8c04
emo-nikoloff <emobg321@gmail.com>
2026-03-21 00:11:55 +0200

    New task

---
+++ C#/3. C# Advanced/OOP/6.1 SOLID - Lab/03. Detail Printer/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace DetailPrinter;
+
+public class Employee
+{
+    public Employee(string name)
+    {
+        this.Name = name;
+    }
+
+    public string Name { get; set; }
+}
+
---
+++ C#/3. C# Advanced/OOP/6.1 SOLID - Lab/03. Detail Printer/Manager.cs
@@ -0,0 +1,12 @@
+﻿namespace DetailPrinter;
+
+public class Manager : Employee
+{
+    public Manager(string name, ICollection<string> documents) : base(name)
+    {
+        this.Documents = new List<string>(documents);
+    }
+
+    public IReadOnlyCollection<string> Documents { get; set; }
+}
+
