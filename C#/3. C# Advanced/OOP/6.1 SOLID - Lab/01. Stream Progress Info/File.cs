9fec4058a7a69e52a26f44a5fff7f2d8ecd4602e
emo-nikoloff <emobg321@gmail.com>
2026-03-20 23:35:01 +0200

    New task

---
+++ C#/3. C# Advanced/OOP/6.1 SOLID - Lab/01. Stream Progress Info/File.cs
@@ -0,0 +1,16 @@
+namespace StreamProgressInfo;
+
+public class File
+{
+    private string name;
+
+    public File(string name, int length, int bytesSent)
+    {
+        this.name = name;
+        this.Length = length;
+        this.BytesSent = bytesSent;
+    }
+
+    public int Length { get; set; }
+    public int BytesSent { get; set; }
+}
---
+++ C#/3. C# Advanced/OOP/6.1 SOLID - Lab/01. Stream Progress Info/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace StreamProgressInfo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
