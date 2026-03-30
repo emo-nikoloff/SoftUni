33e891b248041286c22a741fb943c7d0591d294c
emo-nikoloff <emobg321@gmail.com>
2026-03-30 23:46:27 +0300

    Update

--- C#/3. C# Advanced/OOP/8.1 Unit Testing - Lab/Skeleton.Tests/AxeTests.cs
+++ C#/3. C# Advanced/OOP/8.1 Unit Testing - Lab/Skeleton.Tests/AxeTests.cs
@@ -1,17 +1,34 @@
-namespace Skeleton.Tests
+namespace Skeleton.Tests;
+
+[TestFixture]
+public class AxeTests
 {
-    [TestFixture]
-    public class AxeTests
+    [Test]
+    public void AxeShouldLoseDurabilityAfterEachAttack()
     {
-        [Test]
-        public void Test1()
+        // Arrange
+        Axe axe = new(10, 20);
+        Dummy dummy = new(100, 200);
+
+        // Act
+        axe.Attack(dummy);
+
+        // Assert
+        Assert.That(axe.DurabilityPoints, Is.EqualTo(19));
+    }
+
+    [Test]
+    public void BrokenAxeShouldNotAttack()
+    {
+        // Arrange
+        Axe axe = new(10, 0);
+        Dummy dummy = new(100, 200);
+
+        // Assert
+        Assert.Throws<InvalidOperationException>(() =>
         {
-            Axe axe = new(10, 10);
-            Dummy dummy = new(10, 10);
-
+            // Act
             axe.Attack(dummy);
-
-            Assert.That(axe.DurabilityPoints, Is.EqualTo(9), "Axe Durability doesn't change after attack.");
-        }
+        }, "Axe is broken.");
     }
 }
--- C#/3. C# Advanced/OOP/8.1 Unit Testing - Lab/Skeleton.Tests/DummyTests.cs
+++ C#/3. C# Advanced/OOP/8.1 Unit Testing - Lab/Skeleton.Tests/DummyTests.cs
@@ -1,12 +1,61 @@
-﻿namespace Skeleton.Tests
-{
-    [TestFixture]
-    public class DummyTests
-    {
-        [Test]
-        public void Test1()
-        {
+﻿namespace Skeleton.Tests;
 
-        }
+[TestFixture]
+public class DummyTests
+{
+    [Test]
+    public void DummyShouldLoseHealthAfterAttack()
+    {
+        // Arrange
+        Axe axe = new(10, 20);
+        Dummy dummy = new(100, 200);
+
+        // Act
+        axe.Attack(dummy);
+
+        // Assert
+        Assert.That(dummy.Health, Is.EqualTo(90));
+    }
+
+    [Test]
+    public void DeadDummyShouldNotBeAttacked()
+    {
+        // Arrange
+        Axe axe = new(10, 20);
+        Dummy dummy = new(0, 200);
+
+        // Assert
+        Assert.Throws<InvalidOperationException>(() =>
+        {
+            // Act
+            axe.Attack(dummy);
+        }, "Dummy is dead.");
+    }
+
+    [Test]
+    public void DeadDummyShouldGiveXP()
+    {
+        // Arrange
+        Dummy dummy = new(0, 200);
+
+        // Act
+        int result = dummy.GiveExperience();
+
+        // Assert
+        Assert.That(result, Is.EqualTo(200));
+    }
+
+    [Test]
+    public void AliveDummyShouldNotGiveXP()
+    {
+        // Arrange
+        Dummy dummy = new(100, 200);
+
+        // Assert
+        Assert.Throws<InvalidOperationException>(() =>
+        {
+            // Act
+            int result = dummy.GiveExperience();
+        }, "Target is not dead.");
     }
 }
