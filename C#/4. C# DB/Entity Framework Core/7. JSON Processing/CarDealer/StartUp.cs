ee65416c82362925d7b22616c3fb6c454fe6dfb4
emo-nikoloff <emobg321@gmail.com>
2026-07-13 19:05:34 +0300

    New exercise

---
+++ C#/4. C# DB/Entity Framework Core/7. JSON Processing/CarDealer/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using CarDealer.Data;
+
+namespace CarDealer;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        using CarDealerContext dbContext = new();
+
+        /* Part I. ProductShop
+         * Part I.I. Import - разкоментирайте първия jsonFilePath, първия result и първия jsonFileContent и сменяйте само jsonFileName и името на метода Import...()
+            Задача 1 - Users, 2 - Products, 3 - Categories, 4 - CategoryProducts
+         * Part I.II. Export - разкоментирайте втория jsonFilePath, втория result и втория jsonFileContent и сменяйте само jsonFileName и името на метода Get...()
+            Задача 5 - ProductsInRange, Задача 6 - SoldProducts, Задача 7 - CategoriesByProducts, Задача 8 - UsersWithProducts
+        */
+    }
+}
